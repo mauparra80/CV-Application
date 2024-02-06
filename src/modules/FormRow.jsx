ed6c7a8ab18337ea33e4568086a09d6966d048c3
Mauricio <mauparra80@hotmail.com>
2024-02-06 14:52:42 -0600

    add scroll to left menu and fixed size to display

--- src/modules/FormRow.jsx
+++ src/modules/FormRow.jsx
@@ -3,20 +3,31 @@ import Utils from "./Utils";
 function FormRow({label, updateThisForm, details, index, type}) {
 
   //fix id to match its form.
-  const id = `${type}-${label}`;
+  const id = label;
   //creat a displayed label using label. (replace - with ' ')(it is capitalized using css)
   let displayedLabel = label.replace('_',' ');
 
   return (
     <div className="form-row">
             <label htmlFor={id} className="capitalize" >{displayedLabel}</label>
-            <input 
-              type="text" 
-              name={id} 
-              id={id}
-              onChange={(e) => updateThisForm(e.target.value, label, index)} 
-              value={details[label] || ""}
-              />
+            {label == 'description' ? 
+            <textarea
+            className="form-input"
+            name={id} 
+            id={id}
+            onChange={(e) => updateThisForm(e.target.value, label, index)} 
+            value={details[label] || ""}>
+
+            </textarea> 
+            : <input 
+            className="form-input"
+            type="text"
+            name={id} 
+            id={id}
+            onChange={(e) => updateThisForm(e.target.value, label, index)} 
+            value={details[label] || ""}
+            />}
+            
     </div>
   )
 }
